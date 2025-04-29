609bbb0988676a0be6bb24a61048be7a00e02fed
German Godoy <64043456+GermnGodoy@users.noreply.github.com>
2025-04-29 21:10:20 +0200

    Now I think it will work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'hybrid', // Enable SSR for API routes while keeping the rest of the site static
+  adapter: vercel()
 });
